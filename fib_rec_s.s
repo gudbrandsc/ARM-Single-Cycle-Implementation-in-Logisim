353d2b1ac756dc8e6392dcafcf6a706ba103fc0f
gudbrandsc <gudbrand.schistad@gmail.com>
2018-04-24 22:36:01 +0000

    Small changes to arm code

--- fib_rec_s.s
+++ fib_rec_s.s
@@ -12,27 +12,27 @@ halt:
 fib_rec_s:
 	sub sp, sp, #16
 	str lr, [sp]
-	cmp r0, #0
+	cmp r0, #0 // if n == 0
 	bne l1
 	mov r0, #0
 	b end
 l1:
-	cmp r0, #1
-	bne rec
-	mov r0, #1
-	b end
+	cmp r0, #1 // if n == 1
+	bne rec // if not do recursive call
+	mov r0, #1 // set r0 == 1
+	b end // end execution 
 rec:
-	str r0, [sp, #4]
-	sub r0, r0, #1
-	bl fib_rec_s
+	str r0, [sp, #4] //Store R0 in sp
+	sub r0, r0, #1 // n-1
+	bl fib_rec_s // Call fib_rec_s(n-1)
 	mov r1, r0
 	str r1, [sp, #8]
 	ldr r0, [sp, #4]
-	sub r0, r0, #2
-	bl fib_rec_s
+	sub r0, r0, #2 // n-2
+	bl fib_rec_s // call fib_rec_s(n-2)
 	ldr r1, [sp, #8]
-	add r0, r1, r0
+	add r0, r1, r0 // add fib(n-1) + fib(n-2)
 end:
 	ldr lr, [sp]
 	add sp, sp, #16
-	bx lr
+	bx lr 
--- find_max_s.s
+++ find_max_s.s
@@ -27,21 +27,21 @@ main:
 	mov r0, sp
 	mov r1, #5
 	bl find_max_s
-end:
-	b end
+halt:
+	b halt
 
 find_max_s:
-	ldr r2, [r0]
-	mov r3, #1
+	ldr r2, [r0] // r2 == array[0]
+	mov r3, #1 // r3 == 1
 loop:
-	cmp r3, r1
-	bgt loop_end
-	ldr r12, [r0]
-	cmp r12, r2
-	movgt r2, r12
-	add r0, r0, #4
-	add r3, r3, #1
+	cmp r3, r1 // r3 == r1
+	bgt loop_end // if true end iteration
+	ldr r12, [r0] // load r0 to r12
+	cmp r12, r2 // cmp max with new value
+	movgt r2, r12 // if r12 > r2 then r2 = r12
+	add r0, r0, #4 // Go to next value in array
+	add r3, r3, #1 // i++
 	b loop
 loop_end:
-	mov r0, r2
+	mov r0, r2 // set max value to r0 
 	bx lr
